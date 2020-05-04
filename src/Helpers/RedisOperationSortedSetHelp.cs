a1da6b9bfad39ee9e92f7eb1ad2a82fce5ad2f7b
godzff <godzff@gmail.com>
2020-05-04 23:04:20 +0800

    增加sortset操作

---
+++ src/Helpers/RedisOperationSortedSetHelp.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Options;
+using NewAge.Infra.Exceptions;
+using NewAge.Infra.Extensions;
+using NewAge.Infra.Helpers;
+using NewAge.Redis.Const;
+using NewAge.Redis.Interfaces;
+using NewAge.Redis.Options;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewAge.Redis.Helpers
+{
+    public partial class RedisOperationHelp
+    {
+        /// <summary>
+        /// SortedSet 新增
+        /// </summary>
+        /// <param name="redisKey"></param>
+        /// <param name="member"></param>
+        /// <param name="score"></param>
+        /// <returns></returns>
+        public async Task<bool> SortedSetAddAsync<T>(string key, T value, double score)
+        {
+            if (value == null)
+            {
+                throw new MyException(nameof(value));
+            }
+            return await redisBase.DoSave(db => db.SortedSetAddAsync(RedisPrefixKey.SortedSetKey + key, value.ToJson(), score));
+        }
+        /// <summary>
+        /// 获取SortedSet的数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public async Task<T> SortedSetGetAsync<T>(string key, double score)
+        {
+            var result = await redisBase.DoSave(db => db.SortedSetRangeByScoreAsync(RedisPrefixKey.SortedSetKey + key, score));
+            return result.ToStr().JsonTo<T>();
+        }
+        /// <summary>
+        /// 获取集合中的数量
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public async Task<long> SortedSetLengthAsync(string key)
+        {
+            return await redisBase.DoSave(db => db.SortedSetLengthAsync(RedisPrefixKey.SortedSetKey + key));
+        }
+        /// <summary>
+        /// 移除SortedSet
+        /// </summary>
+        public async Task<bool> SortedSetRemoveAsync<T>(string key, T value)
+        {
+            if (value == null)
+            {
+                throw new MyException(nameof(value));
+            }
+            return await redisBase.DoSave(db => db.SortedSetRemoveAsync(RedisPrefixKey.SortedSetKey + key, value.ToJson()));
+        }
+    }
+}
